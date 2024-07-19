613a9888e82bc2afff59e2acb4b5843281ea591b
Moealsir <mohamedwdalsir@gmail.com>
2024-07-19 17:13:08 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,80 +1,85 @@
 #!/bin/bash
 
-# update and upgrade
+# Update and upgrade
 echo ""
 echo "Updating and upgrading..."
 sudo apt-get update
 # sudo apt-get upgrade -y
 clear
 
-# install git
+# Install git
 echo ""
 echo "Installing git..."
 sudo apt-get install git -y
 clear
 
-# install curl
+# Install curl
 echo ""
 echo "Installing curl..."
 sudo apt-get install curl -y
 clear
 
-# install latest version of nodejs
+# Install nvm (Node Version Manager)
 echo ""
-echo "Installing nodejs..."
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
-nvm install --lts
-nvm use --lts
+echo "Installing nvm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 clear
 
-# install latest version of npm
+# Install specific version of Node.js and npm
 echo ""
-echo "Installing npm..."
-sudo npm install -g npm
+echo "Installing Node.js v20.15.1 and npm v10.7.0..."
+nvm install 20.15.1
+nvm use 20.15.1
+npm install -g npm@10.7.0
 clear
 
-# install sshpass 
+# Install sshpass
 echo ""
 echo "Installing sshpass..."
 sudo apt-get install sshpass -y
+clear
 
-# install trash-cli
+# Install trash-cli
 echo ""
 echo "Installing trash-cli..."
 sudo apt-get install trash-cli -y
 clear
 
-# install python3
+# Install python3
 echo ""
 echo "Installing python3..."
 sudo apt-get install python3 -y
 clear
 
-# install pm2 
+# Install pm2
 echo ""
+echo "Installing pm2..."
 npm install pm2@latest -g
 pm2 startup
 pm2 save
 clear
 
-# install mysql
+# Install MySQL
 echo ""
+echo "Installing MySQL..."
 sudo apt update
 sudo apt install mysql-server -y
 sudo systemctl start mysql.service
-
-# cp dir_navigator.sh to /usr/local/bin
-echo ""
-echo "Copying dir_navigator.sh to /bin..."
-sudo cp dir_navigator.sh /bin/
 clear
 
+# Copy dir_navigator.sh to /usr/local/bin
+echo ""
+echo "Copying dir_navigator.sh to /usr/local/bin..."
+sudo cp dir_navigator.sh /usr/local/bin/
+clear
 
-# create gituser gitmail token variables
-read -p "Enter your Github username: " gituser
-read -p "Enter your Github email: " gitmail
-read -p "Enter your Github token: " token
+# Create gituser, gitmail, token variables
+read -p "Enter your GitHub username: " gituser
+read -p "Enter your GitHub email: " gitmail
+read -sp "Enter your GitHub token: " token
+echo  # To add a new line after entering the token
 
 # Add the variables to ~/.bashrc
 echo "Adding gituser, gitmail, and token to ~/.bashrc..."
@@ -88,11 +93,10 @@ echo "Adding gituser, gitmail, and token to ~/.bashrc..."
     echo ""
 } >> ~/.bashrc
 
-#add file to_bash content to ~/.bashrc
+# Add file to_bash content to ~/.bashrc
 cat to_bash >> ~/.bashrc
 
-
-# ask user if want to install and setup nginx if yes set it up
+# Ask user if they want to install and set up nginx
 read -p "Do you want to install and setup nginx? (y/n): " nginx
 if [ "$nginx" == "y" ]; then
     echo "Installing nginx..."
@@ -103,3 +107,5 @@ if [ "$nginx" == "y" ]; then
     sudo ufw enable
     clear
 fi
+
+echo "Setup complete."
