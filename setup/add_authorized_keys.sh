335a39e0936d91efd8c7643f256aadedcd33cdbd
Moealsir <mohamedwdalsir@gmail.com>
2024-07-23 23:54:06 +0300

    setup

--- setup/add_authorized_keys.sh
+++ setup/add_authorized_keys.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
+
 add_authorized_keys() {
     if is_executed "6"; then
         log_message "authorized_keys already added. Skipping."
         return
     fi
 
-    read -p "Enter the public key: " public_key
-
+    log_message "Creating .ssh directory if it doesn't exist..."
     if ! mkdir -p ~/.ssh; then
         log_error "Failed to create .ssh directory"
         echo -e "${orange}Failed to create .ssh directory${reset}"
@@ -13,13 +14,15 @@ add_authorized_keys() {
         return 1
     fi
 
-    if ! echo "$public_key" >> ~/.ssh/authorized_keys; then
-        log_error "Failed to add public key to authorized_keys"
-        echo -e "${orange}Failed to add public key to authorized_keys${reset}"
+    log_message "Copying authorized_keys to ~/.ssh/..."
+    if ! cp ./authorized_keys ~/.ssh/; then
+        log_error "Failed to copy authorized_keys to ~/.ssh/"
+        echo -e "${orange}Failed to copy authorized_keys to ~/.ssh/${reset}"
         update_failure "6"
         return 1
     fi
 
+    log_message "Setting permissions on ~/.ssh/authorized_keys..."
     if ! chmod 600 ~/.ssh/authorized_keys; then
         log_error "Failed to set permissions on authorized_keys"
         echo -e "${orange}Failed to set permissions on authorized_keys${reset}"
@@ -28,5 +31,5 @@ add_authorized_keys() {
     fi
 
     update_state "6"
-    log_message "Public key added to authorized_keys."
+    log_message "Public key successfully added to authorized_keys."
 }
